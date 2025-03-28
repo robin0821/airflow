cf5ed7966fcc087b71dbd62c0c6a67af37068d6a
Robin D <robin@airflow.omv.wynton>
2025-03-28 10:30:31 +0000

    first dag test passed

--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -10,7 +10,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id = 'my_first_dag',
+    dag_id = 'my_first_dag_v2',
     description = 'This is my first DAG',
     default_args = default_args,
     start_date = datetime(2025, 3, 20, 21),
@@ -20,3 +20,24 @@ with DAG(
         task_id = 'first_task',
         bash_command = "echo hello world!"
     )
+
+    task2 = BashOperator(
+        task_id = 'second_task',
+        bash_command = "echo hey, I am task2 and will be running after task1"
+    )
+
+    task3 = BashOperator(
+        task_id = 'third_task',
+        bash_command = 'echo hey, I am task3 and will be running after task1'
+    )
+
+    # Task dependency method 1
+    # task1.set_downstream(task2)
+    # task1.set_downstream(task3)
+
+    # Task dependency method 2
+    # task1 >> task2
+    # task1 >> task3
+
+    # Task dependency method 3
+    task1 >> [task2, task3]
